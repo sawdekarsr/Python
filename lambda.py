08d3f1c93b23b35a94ad96607114119f6fa9be2d
Swapnil Sawdekar <swapnil.sawdekar@gmail.com>
2017-08-07 11:24:32 +0530

    Update lambda.py
    Adding comments to understand.
    Added below functions
    1. Lambda
    2. filter()

--- lambda.py
+++ lambda.py
@@ -1,9 +1,19 @@
+#####################Simple lambda function#####################
 f = lambda x,y:x*y        #Lambda function for multiplication
 print f(4,5)              #Call lambda function
 
-a = [i+1 for i in range(10)]    #List comprehension
-print a
+#####################List comprehension#####################
+numberList = [i+1 for i in range(10)]    #List comprehension
+print numberList
 
+#####################Using lamda function in list comprehension#####################
 fsquare = lambda x:x*x          #Lambda fuction to calculate square of a number
-b = [fsquare(i) for i in a]     #Use lambda function in list comprehension
-print b
+squareList = [fsquare(i) for i in numberList]     #Use lambda function in list comprehension
+print squareList
+
+#####################Use filter fuction along with lambda#####################
+print filter(lambda i: i>10 and i<60, squareList)  #Use filter() to get numbers in between 10 and 60 from the square of numbers
+
+#####################Write above code in different style. Syntax : filter(function, list)#####################
+ffilterLambda = lambda i: i>10 and i<60          #First define lambda function with filter conditions
+print filter(ffilterLambda, squareList)          #call filter() by passing 1st argument as lambda function and second as data.
